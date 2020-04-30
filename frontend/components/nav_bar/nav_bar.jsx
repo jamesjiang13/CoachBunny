da29463b8fad4a44f3bb169655500065de652b89
James <james.jiang13@gmail.com>
2020-04-30 02:25:11 -0700

    Refactored loggedinnav and loggedoutnav to react components

--- frontend/components/nav_bar/nav_bar.jsx
+++ frontend/components/nav_bar/nav_bar.jsx
@@ -1,45 +1,41 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import classes from './nav_bar.module.css';
-// import logo from '../../../app/assets/images/full_logo.png';
 
-const NavigationBar = ({ currentUser, logout }) => {
-  const loggedInNav = () => (
-    <ul className={classes.navBarUl}>
-      <li key="bookASession"> Book a Session</li>
-      <li key="mySession"> My Sessions</li>
-      <li key="account"> Account</li>
-      <li key="logoutButton">
-        <button type="button" onClick={logout}> Log out </button>
-      </li>
-    </ul>
-  );
 
-  const loggedOutNav = () => (
-    <ul>
-      
-    </ul>
-    <ul className={classes.navBarUl}>
-      <li key="sport">Sports</li>
-      <li key="login">
-        <Link to="/entrypage"> Log in </Link>
-      </li>
-      <button type="button"> Become a Coach </button>
-    </ul>
-  );
+const LoggedInNav = ({ logout }) => (
+  <ul className={classes.navBarUl}>
+    <li key="bookASession"> Book a Session</li>
+    <li key="mySession"> My Sessions</li>
+    <li key="account"> Account</li>
+    <li key="logoutButton">
+      <button type="button" onClick={logout}> Log out </button>
+    </li>
+  </ul>
+);
 
-  return (
-    <header className={classes.mainHeader}>
-      <nav className={classes.navbar}>
-        <Link to="/" className={classes.leftNav}>
-          <img className={classes.imgResponsive} src="assets/full_logo2.png" alt="logo" />
-        </Link>
-        <div className={classes.rightNav}>
-          { currentUser ? loggedInNav() : loggedOutNav() }
-        </div>
-      </nav>
-    </header>
-  );
-};
+const LoggedOutNav = () => (
+  <ul className={classes.navBarUl}>
+    <li key="sport">Sports</li>
+    <li key="login">
+      <Link to="/entrypage"> Log in </Link>
+    </li>
+    <button type="button"> Become a Coach </button>
+  </ul>
+);
+
+const NavigationBar = ({ currentUser, logout }) => (
+  <header className={classes.mainHeader}>
+    <nav className={classes.navbar}>
+      <Link to="/" className={classes.leftNav}>
+        <img className={classes.imgResponsive} src="assets/full_logo2.png" alt="logo" />
+      </Link>
+      <div className={classes.rightNav}>
+        { currentUser ? <LoggedInNav logout={logout} /> : <LoggedOutNav /> }
+      </div>
+    </nav>
+  </header>
+);
+
 
 export default NavigationBar;
