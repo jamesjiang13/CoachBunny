e2b88c096347eec2332a391efa2f2d662af8a7fb
James <james.jiang13@gmail.com>
2020-06-02 17:36:26 -0700

    Added modal for coach selection

--- frontend/components/coaching_session_search/search_results.jsx
+++ frontend/components/coaching_session_search/search_results.jsx
@@ -3,12 +3,23 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import classes from './search_results.module.css';
 import SearchResultItem from './search_result_item';
+import NavBar from '../nav_bar/nav_bar_container';
 // import { OPEN_MODAL, CLOSE_MODAL } from '../../actions/modal_actions';
 
 class SearchResults extends React.Component {
   render() {
     const { results } = this.props;
-    if (results.length === 0) return 'no results';
+    if (results.length === 0) return (
+      <div className={classes.noCoaches}>
+        <NavBar />
+        <div className={classes.noCoachesBody}>
+          <span>We're sorry, your search did not return any coaches.</span>
+          <div className={classes.redoSearch}>
+            <Link to="/main" className={classes.redoSearchLink}>Search Again</Link>
+          </div>
+        </div>
+      </div>
+    )
     return (
       <div>
         <div className={classes.textLogo}>
@@ -20,9 +31,7 @@ class SearchResults extends React.Component {
           {results.map((result) => <SearchResultItem result={result} key={result.id} />)}
         </div>
         <div className={classes.redoSearch}>
-          <button type="button">
-            <Link to="/main" className={classes.redoSearchLink} />
-          </button>
+          <Link to="/main" className={classes.redoSearchLink}>Search Again</Link>
         </div>
       </div>
     );
--- frontend/components/coaching_session_search/search_select_item_container.js
+++ frontend/components/coaching_session_search/search_select_item_container.js
@@ -3,20 +3,17 @@ import { connect } from 'react-redux';
 import { createCoachingSession } from '../../actions/coaching_session_actions';
 import SearchSelectItem from './search_select_item';
 
-const mapState = (state, ownProps) => ({
-  coach: ownProps.history.location.state,
-  userId: state.session.currentUserId,
-  trainingDescription: state.search.searchParams.description,
-});
+const mapState = (state, ownProps) => {
+  return ({
+    coach: ownProps.history.location.state,
+    userId: state.session.currentUserId,
+    description: state.search.searchParams.description,
+  });
+};
 
 const mapDispatch = (dispatch) => ({
   submitForm: (coachingSession) => dispatch(createCoachingSession(coachingSession)),
-  // closeModal: () =>dispatch(closeModal()),
-  // otherForm: (
-  //   <button onClick={() => dispatch(openModal('select'))}>
-  //     Select me
-  //   </button>
-  // )
+
 });
 
 export default withRouter(connect(
