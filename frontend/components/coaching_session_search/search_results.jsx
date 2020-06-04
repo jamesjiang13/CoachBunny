1b8b45ad84e82fff447fbbcff782d10ae04a4b8a
James <james.jiang13@gmail.com>
2020-06-03 23:16:52 -0700

    Added coach filter function

--- frontend/components/coaching_session_search/search_results.jsx
+++ frontend/components/coaching_session_search/search_results.jsx
@@ -4,22 +4,62 @@ import { Link } from 'react-router-dom';
 import classes from './search_results.module.css';
 import SearchResultItem from './search_result_item';
 import NavBar from '../nav_bar/nav_bar_container';
-// import { OPEN_MODAL, CLOSE_MODAL } from '../../actions/modal_actions';
 
 class SearchResults extends React.Component {
-  render() {
-    const { results } = this.props;
-    if (results.length === 0) return (
+  constructor(props) {
+    super(props);
+    this.state = {
+      eliteFilter: false,
+      equipmentFilter: false,
+    };
+    this.noResults = this.noResults.bind(this);
+    this.filterSearchResults = this.filterSearchResults.bind(this);
+    this.toggle = this.toggle.bind(this);
+  }
+
+  toggle(field) {
+    return (e) => {
+      this.setState({
+        [field]: !this.state[field],
+      });
+    };
+  }
+
+  noResults() {
+    return (
       <div className={classes.noCoaches}>
         <NavBar />
         <div className={classes.noCoachesBody}>
-          <span>We're sorry, your search did not return any coaches.</span>
+          <span>We&apos;re sorry, your search did not return any coaches.</span>
           <div className={classes.redoSearch}>
             <Link to="/main" className={classes.redoSearchLink}>Search Again</Link>
           </div>
         </div>
       </div>
     )
+  }
+
+  filterSearchResults() {
+    const { eliteFilter, equipmentFilter } = this.state;
+    const { results } = this.props;
+    
+    switch (true) {
+      case eliteFilter && equipmentFilter:
+        return results.filter((coach) => coach.eliteCoach && coach.equipment);
+      case eliteFilter && !equipmentFilter:
+        return results.filter((coach) => coach.eliteCoach);
+      case !eliteFilter && equipmentFilter:
+        return results.filter((coach) => coach.equipment);
+      default:
+        return results;
+    }
+  }
+
+  render() {
+    const coaches = this.filterSearchResults();
+    const { eliteFilter, equipmentFilter } = this.state;
+    if (coaches.length === 0) return this.noResults();
+
     return (
       <div>
         <div className={classes.textLogo}>
@@ -27,11 +67,26 @@ class SearchResults extends React.Component {
             <img className={classes.imgResponsive} src={window.textLogo} alt="logo" />
           </Link>
         </div>
-        <div className={classes.searchResults}>
-          {results.map((result) => <SearchResultItem result={result} key={result.id} />)}
-        </div>
-        <div className={classes.redoSearch}>
-          <Link to="/main" className={classes.redoSearchLink}>Search Again</Link>
+        <div className={classes.searchContainer}>
+          <div className={classes.filterContainer}>
+            <span>Coach Type</span>
+            <label className={classes.checkboxContainer} onChange={this.toggle('eliteFilter')} >
+              Elite Coach
+              <input type="checkbox" checked={eliteFilter} />
+              <span className={classes.checkmark} />
+            </label>
+            <label className={classes.checkboxContainer} onChange={this.toggle('equipmentFilter')}>
+              Has Equipment
+              <input type="checkbox" checked={equipmentFilter} />
+              <span className={classes.checkmark} />
+            </label>
+          </div>
+          <div className={classes.searchResults}>
+            {coaches.map((result) => <SearchResultItem result={result} key={result.id} />)}
+            <div className={classes.redoSearch}>
+              <Link to="/main" className={classes.redoSearchLink}>Search Again</Link>
+            </div>
+          </div>
         </div>
       </div>
     );
