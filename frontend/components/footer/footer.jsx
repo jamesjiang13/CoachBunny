414643a2225f7ed088c7071f3575cc44a156185b
James <james.jiang13@gmail.com>
2021-01-25 15:21:44 -0800

    change footer links to open new tab

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -5,10 +5,10 @@ const Footer = () => (
   <footer className={classes.footerMain}>
     <div className={classes.footerMainDiv}>
       <h4> Follow us, we&#39;re friendly: </h4>
-      <a href="https://github.com/jamesjiang13/CoachBunny">
+      <a target="_blank" href="https://github.com/jamesjiang13/CoachBunny">
         <img className={classes.imgResponsive} src={window.githubLogo} alt="git" />
       </a>
-      <a href="https://www.linkedin.com/in/james-jiang-b962a245/" className={classes.lnkdLogo}>
+      <a target="_blank" href="https://www.linkedin.com/in/james-jiang-b962a245/" className={classes.lnkdLogo}>
         <img className={classes.imgResponsive} src={window.linkedinLogo} alt="lnkd" />
       </a>
     </div>
